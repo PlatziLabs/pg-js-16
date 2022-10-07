c11bb056a7a134dabf861585c2f25b2f4687b191
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-07 06:11:02 -0500

    save

--- src/exercise.js
+++ src/exercise.js
@@ -1,3 +1,11 @@
 export function trialDivision(number) {
-  // Tu código aquí 👈
+  let interaction = 1;
+  let zeroWastedivision = 0;
+  while(interaction <= number ) {
+    if(number % interaction == 0) {
+      zeroWastedivision = zeroWastedivision + 1;
+    }
+    interaction = interaction + 1;
+  }
+  return zeroWastedivision == 2;
 }
